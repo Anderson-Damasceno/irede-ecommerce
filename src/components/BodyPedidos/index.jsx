5c4ae7a9d45ae82bc1630e167b13d7f35658b723
Anderson Damasceno de Paula <andersondamascenodev@gmail.com>
2024-02-23 15:50:17 -0300

    Pedidos

--- src/components/BodyPedidos/index.jsx
+++ src/components/BodyPedidos/index.jsx
@@ -2,18 +2,37 @@ import CardPedidos from "../CardPedidos";
 
 export default function BodyPedidos(){
     return(
-        <div className="">
-            <button className="bg-orange-500 rounded flex flex-row items-center justify-center gap-4 w-64 h-12">Meus pedidos <span><img src="src\assets\seta.png" alt="" /></span></button>
-            <div className="bg-slate-200 w-64">
+        <>
+        <div className="flex flex-row items-center gap-2 max-md:hidden">
+            <div className="bg-slate-200 rounded w-[250px] h-[135px]">
+                <h1>Meus Pedidos</h1>
+                <div className="border-t-4 border-slate-500"></div>
+                <h1>Minhas Informações</h1>
+            </div>
+            <div className="bg-slate-200 rounded w-[850px] h-[300px]">
+                <div className="flex justify-between"><span>Meus Pedidos</span><span>Status Meus Pedidos</span></div>
+                <ul className="flex justify-between items-center">
+                    <li><CardPedidos/></li>
+                    <h1>Finalizado</h1>
+                </ul>
+
+            </div>
+        </div>
+        <div className=" flex flex-col items-center justify-center md:hidden">
+            <div className="m-4">
+            <button className="bg-orange-500 rounded flex flex-row items-center justify-center gap-4 w-64 h-12 mb-4">Meus pedidos <span><img src="src\assets\seta.png" alt="" /></span></button>
+            <div className="bg-slate-200 w-64 rounded">
                 <h1>Meus pedidos</h1>
                 <ul>
                     <li>
                         <CardPedidos/>
-                        <div className="flex flex-row gap-16"><span>Status</span> <span className="text-green-600 font-bold">Finalizado</span></div>
+                        <div className="flex flex-row gap-16"><span>Status:</span> <span className="text-green-600 font-bold">Finalizado</span></div>
                     </li>
                 </ul>
             </div>
-            
+            </div>
+           
         </div>
+        </>
     )
 }
--- src/components/CardPedidos/index.jsx
+++ src/components/CardPedidos/index.jsx
@@ -3,15 +3,13 @@ export default function CardPedidos(){
         <>
         <div className="flex flex-row">
             <div>
-                <img src="src\assets\tenis.png" alt="" className="w-full" />
+                <img src="src\assets\tenis.png" alt="" className="w-full pl-1" />
             </div>
             <div>
-                <h1 className="text-blue-700 font-bold">Nique Air Surf</h1>
-                <p className="text-lime-700">tenis</p>
-                <h2 className="text-orange-500 font-bold">R$ 200,00</h2>
+                <h1 className="text-blue-700 font-bold pl-1">Nique Air Surf</h1>
+                <p className="text-lime-700 pl-1">tenis</p>
+                <h2 className="text-orange-500 font-bold pl-1">R$ 200,00</h2>
             </div>
-            
-        
         </div>
         </>
     )
